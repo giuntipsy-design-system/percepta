bb0a99142d8042a23807826d430fa959b80caad3
Sergio Ortega <sortegasa@gmail.com>
2026-01-27 02:39:32 +0100

    feat(home): refine hero spacing and use gp-button

--- apps/docs/src/app/pages/home/home.component.ts
+++ apps/docs/src/app/pages/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { GpButtonComponent } from '../../../../../shared/components/button/gp-button.component';
 
 @Component({
   selector: 'gp-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [GpButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private readonly router: Router) {}
+
+  goTo(path: string): void {
+    void this.router.navigateByUrl(path);
+  }
+}
