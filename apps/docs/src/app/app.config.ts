aec936602ef34127662a19bd898a481ef9e8e38b
Sergio Ortega <sortegasa@gmail.com>
2026-01-30 20:00:27 +0100

    theme: override radiobutton in preset

--- apps/docs/src/app/app.config.ts
+++ apps/docs/src/app/app.config.ts
@@ -2,9 +2,42 @@ import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import Lara from '@primeuix/themes/lara';
+import { definePreset } from '@primeuix/themes';
 import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
+
+const PerceptaPreset = definePreset(Lara, {
+  components: {
+    radiobutton: {
+      root: {
+        background: 'transparent',
+        borderColor: '#92A3AC',
+        hoverBorderColor: '#3356CC',
+        focusBorderColor: 'rgba(114,87,255,0.30)',
+        checkedBorderColor: '#3356CC',
+        checkedHoverBorderColor: '#3356CC',
+        checkedFocusBorderColor: 'rgba(114,87,255,0.30)',
+        checkedBackground: '#3356CC',
+        checkedHoverBackground: '#3356CC',
+        disabledBackground: '#DAE4E9',
+        checkedDisabledBorderColor: '#7B8F99',
+        focusRing: {
+          width: '1px',
+          style: 'solid',
+          color: 'rgba(114,87,255,0.30)',
+          offset: '0',
+          shadow: '0 0 0 2px rgba(114,87,255,0.2)'
+        }
+      },
+      icon: {
+        checkedColor: '#F3F6FF',
+        checkedHoverColor: '#F3F6FF',
+        disabledColor: '#7B8F99'
+      }
+    }
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,7 +46,7 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({
       ripple: true,
       theme: {
-        preset: Lara,
+        preset: PerceptaPreset,
         options: {
           darkModeSelector: 'none'
         }
