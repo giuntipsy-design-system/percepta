7c5a3e50150bec63bd08e356d5ac42f500599119
Sergio Ortega <sortegasa@gmail.com>
2026-02-05 15:55:42 +0100

    Refine VOL home module layout

--- apps/docs/src/app/pages/prototypes/VOL/home/vol-home.component.ts
+++ apps/docs/src/app/pages/prototypes/VOL/home/vol-home.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { GpButtonComponent } from '../../../../../../../shared/components/button/gp-button.component';
 import { GpIconComponent } from '../../../../../../../shared/components/icon/gp-icon.component';
+import { GpInputComponent } from '../../../../../../../shared/components/input/gp-input.component';
 
 @Component({
   selector: 'percepta-vol-home',
   standalone: true,
-  imports: [CommonModule, GpButtonComponent, GpIconComponent],
+  imports: [CommonModule, GpButtonComponent, GpIconComponent, GpInputComponent],
   templateUrl: './vol-home.component.html',
   styleUrl: './vol-home.component.scss'
 })
